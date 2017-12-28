3bf5313759241db84cc54d2355798a93f005dc39
姜忠良 <zhongliang@codenewman.com>
2017-12-28 20:46:02 +0800

    debug

--- movie_spider/movie_spider/items.py
+++ movie_spider/movie_spider/items.py
@@ -21,3 +21,14 @@ class CollectipsIpItem(scrapy.Item):
     TYPE            = scrapy.Field()
     SPEED           = scrapy.Field()
     LAST_CHECK_TIME = scrapy.Field()
+
+
+class Vip1905Item(scrapy.Item):
+    name = scrapy.Field()  # 名字
+    data_id = scrapy.Field()  # 数据ID
+    score = scrapy.Field()  # 评分
+    url = scrapy.Field()  # 播放链接
+    intro = scrapy.Field()  # 介绍
+    tag_hover = scrapy.Field()  # 标签
+    protagonist = scrapy.Field()  # 主演
+    plot = scrapy.Field()  # 剧情
