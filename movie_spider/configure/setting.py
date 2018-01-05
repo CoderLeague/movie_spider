e76ebd22e6392031bb90565cf5e0251fb680296b
Zhongliang.Jiang <jiangzhongliang@liquidnetwork.com>
2018-01-05 16:20:40 +0800

    增加线上用户信息

--- movie_spider/configure/setting.py
+++ movie_spider/configure/setting.py
@@ -5,13 +5,15 @@ Created on 2018年1月2日
 
 全局配置信息
 
-@author: codenewman
+@author: zhongliang
 '''
 
 ONLINE_MONGODB_SERVER     = "172.17.33.176"
 ONLINE_MONGODB_PORT       = 30001
 ONLINE_MONGODB_DB         = "feed"
 ONLINE_MONGODB_COLLECTION = "capture_mysite_video"
+ONLINE_MONGODB_USERNAME   = "liquid"
+ONLINE_MONGODB_PASSWORD   = "n3tw0rk"
 
 
 # debug configure
@@ -20,3 +22,5 @@ ONLINE_MONGODB_COLLECTION = "capture_mysite_video"
 # ONLINE_MONGODB_PORT       = 27017
 # ONLINE_MONGODB_DB         = "local"
 # ONLINE_MONGODB_COLLECTION = "capture_mysite_video"
+# ONLINE_MONGODB_USERNAME     = None
+# ONLINE_MONGODB_PASSWORD     = None
