7213cf98214969ddb08b469a53cd2a63d9e26153
Zhongliang.Jiang <jiangzhongliang@liquidnetwork.com>
2018-01-07 21:21:53 +0800

    Revert "Revert "实现mongo dao""
    This reverts commit 69d6baef72482dbf4a942a18ebd684238b087f1d.

---
+++ movie_spider/debugger/mongo_dao_debug.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+@author: codenewman
+'''
+from dao.mongo_dao import get_mongo
+
+mongo = get_mongo()
+mongo = get_mongo()
+mongo = get_mongo()
+mongo = get_mongo()
+
+mongo.server_info()
